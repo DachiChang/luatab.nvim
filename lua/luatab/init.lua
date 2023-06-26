c384a3f46ae53615ef269cb6dc8014d90b781990
Dachi Chang <dachi.chang@idtechproducts.com>
2023-06-26 14:02:53 +0800

    not to need show close symbol in tabline

--- lua/luatab/init.lua
+++ lua/luatab/init.lua
@@ -90,9 +90,9 @@ M.tabline = function()
         line = line .. M.cell(i)
     end
     line = line .. '%#TabLineFill#%='
-    if vim.fn.tabpagenr('$') > 1 then
-        line = line .. '%#TabLine#%999XX'
-    end
+    -- if vim.fn.tabpagenr('$') > 1 then
+    --     line = line .. '%#TabLine#%999XX'
+    -- end
     return line
 end
 
